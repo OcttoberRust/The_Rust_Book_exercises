f05a4d964fabf1f9d12af8cd2219c33f6c64895f
octtoberrust <octtoberrust@outlook.com>
2024-10-13 19:22:22 -0500

    small pig_latin_converter change

--- Chapter9/pig_latin_converter/src/main.rs
+++ Chapter9/pig_latin_converter/src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    //Take in a string
+    //if word starts with a vowel, add hay to the end
+    //else search for first consonant
+    //add consonant to end of word and add "ay"
+    //keep in mind UTF-8
+
+
 }
