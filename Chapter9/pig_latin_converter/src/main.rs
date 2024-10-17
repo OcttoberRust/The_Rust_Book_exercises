961863ab822008ee98486d805f8cb1a9f181c01f
octtoberrust <octtoberrust@outlook.com>
2024-10-16 21:10:42 -0500

    updating pig latin exercise

--- Chapter9/pig_latin_converter/src/main.rs
+++ Chapter9/pig_latin_converter/src/main.rs
@@ -2,10 +2,19 @@ use std::io;
 
 fn main() {
     //Take in a string
+    let mut input = String::new();
+    
+    println!("Please type in a phrase to convert to Pig Latin. ");
+    
+    io::stdin().read_line(&mut input).expect("Failed to read line.");
+
+    println!("You typed: {}", input);
+
+    for word in input.split_whitespace() {
+        println!("{}", word);
+    }
+
     //if word starts with a vowel, add hay to the end
-    //else search for first consonant
-    //add consonant to end of word and add "ay"
+    //add first consonant to end of word and add "ay"
     //keep in mind UTF-8
-
-
 }
