a12e823175dbe3d075ba38bc7cb8f2f9f206b846
octtoberrust <octtoberrust@outlook.com>
2024-10-21 21:03:20 -0500

    commenting department-employee program

--- Chapter9/employee_names/src/main.rs
+++ Chapter9/employee_names/src/main.rs
@@ -1,3 +1,17 @@
+use std::collections::HashMap;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut input = String::new();
+
+
+    //ask the user if they would like to enter a employee/department or exit the program
+    //if condition for entering employee 
+    //send input string to function to parse input and return employee name and department name
+    //statement to insert department name (key)
+    //statement to add employee by department name (might be able to consolidate)
+    //else condition to exit program
+
 }
+
