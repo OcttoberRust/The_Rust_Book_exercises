c323543b287f02f6c1ef95e25f00f89c106939c2
octtoberrust <octtoberrust@outlook.com>
2024-10-22 20:31:54 -0500

    improving the department-employee exercise

--- Chapter9/employee_names/src/main.rs
+++ Chapter9/employee_names/src/main.rs
@@ -4,8 +4,13 @@ use std::io;
 fn main() {
 
     let mut input = String::new();
+    let mut map: HashMap::<String, String>;
 
+    println!("Please enter the department name and employee name in this format: 
+             \'Add *Name* to *Department*\'"); 
+    io::stdin().read_line(&mut input);
 
+    let employee = parse_name(&input); 
     //ask the user if they would like to enter a employee/department or exit the program
     //if condition for entering employee 
     //send input string to function to parse input and return employee name and department name
@@ -15,3 +20,25 @@ fn main() {
 
 }
 
+struct Employee {
+    name: String,
+    department: String
+}
+
+fn parse_name(input: &String) -> Employee {
+    let mut parsed_string = input.split_whitespace();
+
+    let mut _name = String::new();
+    let mut _department = String::new();
+
+    if let (Some(second), Some(fourth)) = (parsed_string.nth(1), parsed_string.nth(2)) {
+
+        _name = String::from(second);
+        _department = String::from(fourth);
+    };
+
+    Employee {
+        name: _name,
+        department: _department,
+    }
+}
