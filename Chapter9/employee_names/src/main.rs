a141379cdcc9665aa1fdfac5c9ae60f0e5401013
octtoberrust <octtoberrust@outlook.com>
2024-10-23 19:57:50 -0500

    small change to test insert with maps

--- Chapter9/employee_names/src/main.rs
+++ Chapter9/employee_names/src/main.rs
@@ -4,13 +4,14 @@ use std::io;
 fn main() {
 
     let mut input = String::new();
-    let mut map: HashMap::<String, String>;
+    let mut map: HashMap::<String, String> = HashMap::new();
 
     println!("Please enter the department name and employee name in this format: 
              \'Add *Name* to *Department*\'"); 
     io::stdin().read_line(&mut input);
 
     let employee = parse_name(&input); 
+    map.insert(employee.name, employee.department);
     //ask the user if they would like to enter a employee/department or exit the program
     //if condition for entering employee 
     //send input string to function to parse input and return employee name and department name
